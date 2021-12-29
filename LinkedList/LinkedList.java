f6340993fedd47a1a798265f0e669633268d2126
Abhishek <abhishek.ab09@gmail.com>
2021-12-29 17:09:23 +0530

    mergesort

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -39,38 +39,128 @@ public class LinkedList{
         if(head == null){
             return;
         }
-        else if(head.data == data){
+        //special case
+        if(head.data == data){
             head = head.next;
             return;
         }
         Node prev = head;
-        Node temp = head;
-        do{
-            if(temp.data == data){
-                prev.next = temp.next;
-                //temp is dereferrenced hence will be collected by garbage collector
+        Node current = head.next;
+        while(current != null){
+            if(current.data == data){
+                prev.next = current.next;
                 break;
             }
-            temp = temp.next;
-        }while(temp != null);
+            prev = current;
+            current = current.next;
+        }
+    
+    }
+
+    public void reverse(){
+        if(head == null)
+            return;
+        Node prev = null;
+        Node current = head;
+        while(current.next != null){
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        current.next = prev;
+        head = current;
+    }
+    //reverse first k nodes
+    public void kReverse(int k){
+        if(head == null || k == 1)
+            return;
+        Node prev = null;
+        Node current = head;
+        int cnt = 1;
+        while(current.next != null && cnt < k){
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+            cnt++;
+        }
+        head.next = current.next;
+        current.next = prev;
+        head = current;     
+    }
+    public static Node midNode(Node h ){
+        if(h == null){
+            return null;
+        }
+        Node slow = h;
+        Node fast = h.next;
+        while(fast != null && fast.next != null ){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    private static Node merge(Node l, Node r){
+        if(l == null) return r;
+        if(r == null) return l;
+        Node c =  null;
+        if(l.data < r.data){
+            c = l;
+            c.next = merge(l.next, r);
+        }else{
+           c = r; 
+           c.next = merge(l, r.next);
+        }
+        return c;
+    }
+    public static Node mergeSort(Node h){
+        if(h == null)return null;
+        if(h.next == null) return h;
+        Node mid = midNode(h);
+        Node right = mid.next;
+        mid.next = null;
+        Node l = mergeSort(h);
+        Node r = mergeSort(right);
+        return merge(l, r);
     }
     public static void main(String[] args){
         LinkedList ll = new LinkedList();
-        ll.print();
+        // ll.print();
+        // ll.insert(1);
+        // ll.print();
+        // ll.insert(3);
+        // ll.insert(2);
+        // ll.print();
+        // ll.delete(3);
+        // ll.print();
+        // ll.delete(2);
+        // ll.print();
+        // ll.insert(5);
+        // ll.print();
+        // ll.delete(1);
+        // ll.print();
+        // ll.delete(5);
+        // ll.print();
         ll.insert(1);
+        ll.insert(13);
+        ll.insert(12);
+        ll.insert(15);
+        ll.insert(14);
+        ll.insert(17);
         ll.print();
-        ll.insert(3);
-        ll.insert(2);
+        //ll.reverse();
+        ll.head = LinkedList.mergeSort(ll.head);
         ll.print();
-        ll.delete(3);
-        ll.print();
-        ll.delete(2);
-        ll.print();
-        ll.insert(5);
-        ll.print();
-        ll.delete(1);
-        ll.print();
-        ll.delete(5);
-        ll.print();
+        // ll.kReverse(5);
+        // ll.print();
+        // ll.delete(1);
+        // ll.print();
+        // ll.delete(13);
+        // ll.print();
+        // ll.delete(15);
+        // ll.print();
+        // ll.delete(10);
+        // ll.print();
     }
 }
